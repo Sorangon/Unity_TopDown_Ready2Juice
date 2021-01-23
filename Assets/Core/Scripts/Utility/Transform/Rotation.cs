631f57fbf113f5e225ab5c7baa9a852f1605a171
JulienDelaunay <julien.delaunay034@gmail.com>
2021-01-23 01:21:36 +0100

    Implemented base meshes for ships

---
+++ Assets/Core/Scripts/Utility/Transform/Rotation.cs
@@ -0,0 +1,15 @@
+﻿namespace TopDownShooter.Utility {
+    using UnityEngine;
+
+    public class Rotation : MonoBehaviour {
+        [SerializeField] private Vector3 eulerRotation = Vector3.up;
+
+        Quaternion currentRot = Quaternion.identity;
+
+        void Update() {
+            currentRot *= Quaternion.Euler(eulerRotation * Time.deltaTime);
+            transform.rotation = currentRot;
+        }
+    }
+
+}
